6028c50252207707f2326adf5cd7736ad628cf09
Jesper Eskilson <jesperes@seupp-jesper.(none)>
2008-07-08 23:07:32 +0200

    added find/2 with exclude argument

--- erlang-build-tool/findtree.erl
+++ erlang-build-tool/findtree.erl
@@ -7,3 +7,14 @@ find(Dir) ->
     fold_files(Dir, ".*", true, fun(F,AccIn) -> [F|AccIn] end, []).
 
 
+find(Dir, Exclude) ->
+    fold_files(Dir, ".*", true, 
+	       fun(F,AccIn) -> 
+		       case regexp:matches(F, Exclude) of
+			   {match, [{_,_}]} ->
+			       AccIn;
+			   _ ->
+			       [F|AccIn] 
+		       end
+	       end,
+	       []).
