b83c9a36ff7b4722098133e8c9a1c26d76f65912
Jesper Eskilson <jesperes@seupp-jesper.ad.iar.com>
2008-10-23 14:44:18 +0200

    misc

--- timetrack.rb
+++ timetrack.rb
@@ -19,7 +19,8 @@ def make_time(date, tod)
   array[2] = hh
   array[1] = mm
   array[0] = 0
-  return Time.local(*array)
+  t = Time.local(*array)
+  return t
 end
 
 # Return time of day as an integer
@@ -61,9 +62,10 @@ class WorkBlock
     @key = key ? key : Digest::SHA1.hexdigest(Time.now.to_f.to_s)
   end
 
+  # Returns the duration in hours
   def duration
     if @to and @from
-      return @to - @from
+      return (@to - @from)/3600.0
     else
       nil
     end
@@ -147,7 +149,7 @@ class WorkDB
     end
 
     puts "Current date: #{@current_date.strftime("%Y-%m-%d")}"
-    
+    puts "Day length: #{hour_decimal_to_hours_minutes(normal_daylength)}"
     objs = @yaml_obj["workblocks"]
     @workblocks = []
     objs.each do |o|
@@ -164,8 +166,6 @@ class WorkDB
     
     @current_workblock = @wbmap[@yaml_obj["current"]]
 
-    puts "Loaded #{@workblocks.length} records from #{@@WORKDB}"
-    
     Thread.new(self) do |workdb|
       begin
         puts "Spawned deamon thread"
@@ -181,7 +181,7 @@ class WorkDB
   
   def save
     puts "Saving #{@workblocks.length} records to #{@@WORKDB}"
-
+    
     yaml_obj = { 
       "workblocks" => @workblocks.collect { |wb| wb.to_yaml },
       "current" => @current_workblock ? @current_workblock.key : nil,
@@ -245,6 +245,16 @@ class WorkDB
     @config["defaults"]["lunch"].collect do |s| s.to_i end
   end
 
+  def normal_daylength
+    lunch = get_lunch
+    lunch_length = (make_time(@current_date, lunch[1]) - 
+                    make_time(@current_date, lunch[0]))
+
+    start = make_time(@current_date, get_daybegin)
+    stop = make_time(@current_date, get_dayend)
+    return (stop - start - lunch_length)/3600
+  end
+  
   def parse_command(cmd)
     if not cmd or cmd.strip.length == 0
       return
@@ -260,6 +270,20 @@ class WorkDB
       puts msg
       puts msg.backtrace.join("\n")
     end
+  end
+
+  def is_work(what)
+    @config["notwork"].each do |notwork|
+      if what == notwork
+        return false
+      end
+    end
+
+    return true
+  end
+
+  def same_day(t0, t1)
+    return t0.strftime("%x") == t1.strftime("%x")
   end
 
   def help(args = [])
@@ -280,14 +304,74 @@ class WorkDB
     work(args)
   end
 
-  def report(args = [])
+  def hour_decimal_to_hours_minutes(hours)
+    hh = hours.to_i
+    mm = (hours - hh) * 60.0
+    return sprintf("%d:%02d", hh, mm)
+  end
+
+  def worked_today()
+    hours = 0.0
     @workblocks.each do |wb|
-      duration = wb.duration
-      if duration
-        puts "#{wb.what}: #{format_duration(wb.duration)} " + 
-          "(#{short_date(wb.from)} => #{short_date(wb.to)})"
-      else
-        puts "#{wb.what}: active (#{short_date(wb.from)} =>"
+      next unless same_day(wb.from, @current_date)
+      if is_work(wb.what)
+        hours += wb.duration
+      end
+    end
+    return hours
+  end
+
+  def report(args = [])
+    type = args.shift
+    if not type
+      type == "full"
+    else
+      type = type.strip
+    end
+    
+    if type == "daily"
+      whatmap = {}
+      whatmap.default = []
+      
+      puts "Report for #{@current_date}"
+
+      daybegin = 9999
+      dayend = 0
+      hours_worked = 0.0
+
+      # Print a summary for the current day
+      @workblocks.each do |wb|
+        next unless same_day(wb.from, @current_date)
+        whatmap[wb.what] = whatmap[wb.what] + [wb]
+      end
+
+      whatmap.keys.sort.each do |what|
+        puts "#{what}"
+        blocks = whatmap[what].each do |wb|
+          from = get_tod(wb.from)
+          to = get_tod(wb.to)
+
+          puts "   #{from} => #{to}"
+          daybegin = [from, daybegin].min
+          dayend = [to, dayend].max
+
+          if is_work(what)
+            hours_worked += wb.duration
+          end
+        end
+      end
+      
+      puts "Total day: #{daybegin} => #{dayend}"
+      puts "Total hours worked: #{hour_decimal_to_hours_minutes(hours_worked)}"
+    else
+      @workblocks.each do |wb|
+        duration = wb.duration
+        if duration
+          puts "#{wb.what}: #{format_duration(wb.duration)} " + 
+            "(#{short_date(wb.from)} => #{short_date(wb.to)})"
+        else
+          puts "#{wb.what}: active (#{short_date(wb.from)} =>"
+        end
       end
     end
   end
@@ -342,7 +426,12 @@ class WorkDB
   def run
     while true
       begin
-        prompt = @current_date.strftime("%Y-%m-%d> ")
+        date = @current_date.strftime("%Y-%m-%d")
+        time_left_today = normal_daylength - worked_today
+        prompt = sprintf("%s (worked %s, %s left)> ",
+                         date,
+                         hour_decimal_to_hours_minutes(worked_today),
+                         hour_decimal_to_hours_minutes(time_left_today))
         parse_command(Readline.readline(prompt))
       rescue Interrupt
         stop
