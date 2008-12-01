ec34f691713f5a8b32eae4388c50173150ac2cf1
Jesper Eskilson <jesper@eskilson.se>
2008-12-01 22:44:23 +0100

    misc

---
+++ projecteuler/problem38.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+bool
+is_pandigital(int n)
+{
+  int digits[10];
+  
+  for (int n0 = n; n0 > 0; n0 /= 10) {
+    digits[n0 % 10]++;
+  }
+  
+  for (int i = 1; i <= 9; i++) {
+    if (digits[i] != 1)
+      return false;
+  }
+
+  return true;
+}
+
+int
+main()
+{
+  int max = 0;
+
+  for (int i = 192; i < 999999; i++) {
+    char buf[20] = { 0 };
+    int len = 0;
+
+    if (i % 100000 == 0) {
+      cout << "i = " << i << endl;
+    }
+
+    int pandigital = 0;
+    int j;
+    for (j = 1; true; j++) {
+      sprintf(buf + len, "%d", j * i);
+      int p = atol(buf);
+      if (p >= 1000000000) {
+        break;
+      }
+   
+      pandigital = p;
+      len = strlen(buf);
+    }
+    
+    cout << "Checking: " << pandigital << endl;
+
+    if (is_pandigital(pandigital) and (pandigital > max)) {
+      max = pandigital;
+      cout << "New max: " << pandigital << " (" << i << ", " << j-1 << ")" << endl;
+    }
+  }
+
+  return 0;
+}
