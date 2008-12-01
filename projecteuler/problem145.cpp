116ba16fa2670f7f2045d8c520c50f2fc5006227
Jesper Eskilson <jesper@eskilson.se>
2008-12-01 21:35:27 +0100

    misc

--- projecteuler/problem145.cpp
+++ projecteuler/problem145.cpp
@@ -10,54 +10,79 @@ using namespace std;
 
 static clock_t t0 = 0;
 static char buf[20];
-
-inline void to_str(int i)
-{
-  sprintf(buf, "%d", i);
-}
-
 static char rbuf[20];
+const int LIMIT = 1000000000;
 
 int
 main() 
 {
-  int totalsum = 0;
+  int num_reversibles = 0;
   t0 = clock();
   
-  for (int n = 1; n < 1000000; n++) {
+  bool first_digit_even = false;
+  int nlen = 2;                 // length of n
+  int nmag = 10;                // current magnitude of n
+  int nnext = 100;              // when is next increase in nlen?
+
+  for (int n = 10; n < LIMIT; n++) {
+#if 0
+    cout << "n = " << n << endl;
+    cout << "nmag = " << nmag << endl;
+    cout << "nnext = " << nnext << endl;
+    cout << "nlen = " << nlen << endl;
+#endif
+
+    if (n == nnext) {
+      // We've just jumped from 9... to 10...
+      nlen++;
+      nmag *= 10;
+      nnext *= 10;
+      first_digit_even = false;
+
+      // ...but reverse(n) will have a leading zero, so skip it.
+      continue;
+
+    } else if (n % nmag == 0) {
+      // When n % nmag == 0, the left-most digit has just shifted from
+      // even to odd (or vice versa).
+      // cout << "First digit switching evenness: " << n << endl;
+      first_digit_even = ((n / nmag) % 2 == 0);
+      continue;
+    }
+    
     if (n % 10 == 0)
       continue;
     
-    to_str(n);
-    int len = strlen(buf); 
-    
-    // If first and last digits are both odd or both even, then n +
-    // reverse(n) will be even, so we can return early.
-    if ((buf[0] % 2) == (buf[len-1] % 2))
+    bool last_digit_even = (n % 2 == 0);
+    if (first_digit_even == last_digit_even)
       continue;
-    
-    for (int i = 0; i < len; i++) {
-      rbuf[len - i - 1] = buf[i];
-    } 
-    
-    rbuf[len] = 0;
-    int sum = n + atol(rbuf);  
-    
-    to_str(sum);
-    len = strlen(buf);
-    for (int i = 0; i < len; i++) {
-      if (buf[i] % 2 == 0) {
-        goto skip;
-      }
-    }
 
-    totalsum += sum;
-  skip:
-    continue;
+    sprintf(buf, "%d", n);
+    for (int i = 0, revi = nlen - 1; revi >= i; i++, revi--) {
+      int revi = nlen - i - 1;
+      rbuf[i] = buf[revi];
+      rbuf[revi] = buf[i];
+    }
+    
+    // cout << "buf = " << buf << ", rbuf = " << rbuf << endl;
+    
+    int revsum = n + atol(rbuf);
+    int all_odd = true;
+    while (revsum > 0) {
+      if ((revsum % 2) == 0) {
+        all_odd = false;
+        break;
+      }
+      
+      revsum = revsum / 10;
+    }
+    
+    num_reversibles += all_odd;
   }
   
   t0 = (clock() - t0);
   
-  cout << "Reversible numbers: " << totalsum << endl;
-  cout << "Time: " << ((double)t0 / CLOCKS_PER_SEC) << endl;
+  cout << "Reversible numbers: " << num_reversibles << endl;
+  cout << "Time per " << LIMIT << " iterations: " << 
+    ((double)t0 / CLOCKS_PER_SEC) << " seconds " << endl;
 }
