7c612da8898a1e99009c4fb1663016fc975c6ac2
Jesper Eskilson <jesper.eskilson@iar.com>
2008-12-04 10:40:19 +0100

    misc

--- scripts/checkout-coreide-externals.sh
+++ scripts/checkout-coreide-externals.sh
@@ -5,7 +5,13 @@ svn propget svn:externals http://svn.iar.se/ide/platform/trunk | while read f; d
     if [ "$d" ]; then
 	rev=`echo "$f" | awk '{ print $2; }'`
 	url=`echo "$f" | awk '{ print $3; }'`
-	echo "Checking out $url ($rev) -> $d"
-	svn checkout -q $rev $url $d
+	
+	if [ -d "$d" ]; then
+	    echo "Updating $d"
+	    (cd $d && svn switch $rev $url)
+	else
+	    echo "Checking out $url ($rev) -> $d"
+	    svn checkout -q $rev $url $d
+	fi
     fi
 done
