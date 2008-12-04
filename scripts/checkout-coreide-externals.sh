21969585adf5203767b8a3814cb2f63f44c997ef
Jesper Eskilson <JesperEs@seupp-jesper2.ad.iar.com>
2008-12-04 11:22:49 +0100

    misc

--- scripts/checkout-coreide-externals.sh
+++ scripts/checkout-coreide-externals.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-svn propget svn:externals http://svn.iar.se/ide/platform/trunk | while read f; do
+function checkout_external()
+{
     d=`echo "$f" | awk '{ print $1; }'`
     if [ "$d" ]; then
 	rev=`echo "$f" | awk '{ print $2; }'`
 	url=`echo "$f" | awk '{ print $3; }'`
 	
 	if [ -d "$d" ]; then
-	    echo "Updating $d"
-	    (cd $d && svn switch $rev $url)
+	    echo "svn switch $rev $url"
+	    (cd $d && svn switch -q $rev $url)
 	else
-	    echo "Checking out $url ($rev) -> $d"
+	    echo "svn checkout -q $rev $url $d"
 	    svn checkout -q $rev $url $d
 	fi
     fi
+}
+
+svn propget svn:externals http://svn.iar.se/ide/platform/trunk | while read f; do
+    checkout_external "$d"
 done
+
+pushd . >/dev/null
+mkdir -p Jkc/ARM
+cd Jkc/ARM
+svn propget svn:externals http://svn.iar.se/ide/platform/trunk/Jkc/ARM | while read f; do
+    checkout_external "$d"
+done
+popd >/dev/null
+
