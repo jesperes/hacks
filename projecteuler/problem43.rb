0c4d35699ef881a04f5c664701bf44b01f006893
Jesper Eskilson <jesper.eskilson@iar.com>
2008-12-25 22:12:18 +0100

    solution to problem43

--- projecteuler/problem43.rb
+++ projecteuler/problem43.rb
@@ -7,16 +7,44 @@ end
 # such that the first three integers form a number not divisble by the
 # given prime and the entire integer does not form a pandigital
 # number.
-def prepend(list, prime)
+def filter(list, prime)
   newlist = []
-  lists.each do |n|
+  list.each do |n|
+    # puts ">>>" + n.inspect
     (0..9).each do |i|
-      all = i.to_s + n.to_s
-      next if non_pandigital(all)
-      next unless all[0..2].to_i % prime == 0
+      all = i.to_s + n
+      if non_pandigital(all)
+        # puts "not pandigital: #{all}"
+        next
+      end
+
+      if all[0..2].to_i % prime != 0
+        # puts "#{all}: not divisible by #{prime}"
+        next
+      end
+
       newlist << all
     end
   end
   return newlist
 end
+
+def initial_list()
+  list = []
+  1000.times do |n|
+    next if n % 17 != 0
+    s = sprintf("%03d", n)
+    next if non_pandigital(s)
+    list << s
+  end
+  return list
+end
+
+list = initial_list
+primes = [1,2,3,5,7,11,13]
+primes.reverse.each do |prime|
+  list = filter(list, prime)
+end
+puts list.inject {|n, sum| sum.to_i + n.to_i}
+
 
