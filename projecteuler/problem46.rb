8d72cb8b31fd95260a694529c1fd800d136aeb28
Jesper Eskilson <jesper@eskilson.se>
2008-12-29 08:19:03 +0100

    misc

--- projecteuler/problem46.rb
+++ projecteuler/problem46.rb
@@ -1,7 +1,29 @@
 require 'eulerlib'
 limit = ARGV.shift.to_i
+
 puts "Sieving..."
 primes = sieve(limit)
+puts "Primes: #{primes.length}"
+
+puts "Calculating double squares..."
+dsqlist = []
+n = 1
+begin
+  dsq = 2 * n * n
+  dsqlist[n] = dsq
+  n += 1
+end while dsq < limit
+puts "Double squares: #{dsqlist.length}"
 
 max = 0
+primes.each do |p|
+  numbers = Array.new(p - max)
+  max.upto(p) do |n|
+    dsqlist.each do |dsq|
+      m = p + dsq
+      if m > max and m <
+      numbers[n - max]
+    end
+  end
+end
 
