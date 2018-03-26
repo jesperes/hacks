82bdd10c14b38d667fe06f6403f397f05e6a90ed
Jesper Eskilson <jesper.eskilson@iar.com>
2018-03-26 12:51:59 +0200

    Remove misc. old junk

--- cutil-test/Debug/src/runtests.d
+++
@@ -1,7 +0,0 @@
-src/runtests.d src/runtests.o: ../src/runtests.c \
-  E:/hacks/libcutil/include/strbuf.h \
-  E:/hacks/libcutil-test/include/unittest.h
-
-E:/hacks/libcutil/include/strbuf.h:
-
-E:/hacks/libcutil-test/include/unittest.h:
